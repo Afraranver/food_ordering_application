7cd85af9808811c3aabe4c3dc84ea99fdd5eb696
Afraranver <44699401+Afraranver@users.noreply.github.com>
2024-12-11 16:56:26 +0530

    Add models
    fixing read fuctions

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-
+import 'package:food_ordering_application/src/load_dataset';
+import 'dart:developer';
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
+import 'dart:io';
 
 void main() async {
   final settingsController = SettingsController(SettingsService());
@@ -10,4 +12,26 @@ void main() async {
   await settingsController.loadSettings();
 
   runApp(MyApp(settingsController: settingsController));
+
+  const jsonFilePath = 'assets/menu_data.json';
+  final loader = JSONLoader(jsonFilePath);
+
+  try {
+    log('Current Directory: ${Directory.current.path}');
+    // Load Menus
+    final menus = await loader.loadMenus();
+    log('Menus loaded successfully.', name: 'Main');
+    for (var menu in menus) {
+      log('Menu: ${menu.title}', name: 'Main');
+    }
+
+    // Load Categories
+    final categories = await loader.loadCategories();
+    log('Categories loaded successfully.', name: 'Main');
+    for (var category in categories) {
+      log('Category: ${category.title}', name: 'Main');
+    }
+  } catch (e) {
+    log('Error: $e', level: 1000, name: 'Main');
+  }
 }
